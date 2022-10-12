d39ec2ca6a80e72b36955f4be4d83367986ae5c9
dendonpaul <dendonpaul@gmail.com>
2022-10-12 15:36:34 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-2xl bg-red-500 text-sky-500 font-bold underline">
+      Hello world!
+    </h1>
+  );
 }
 
 export default App;
