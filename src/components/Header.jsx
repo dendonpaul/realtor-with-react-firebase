bcf66833a1cd4cb2480f34401575d2ca0b1a5682
dendonpaul <dendonpaul@gmail.com>
2022-10-18 15:08:34 +0530

    dynamic sigin/profile link update in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 const Header = () => {
+  const [pageState, setPageState] = useState("Sign In");
   const location = useLocation();
   const navigate = useNavigate();
+  const auth = getAuth();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) setPageState("Profile");
+      else setPageState("Sign In");
+    });
+  }, [auth]);
   const pathMatchRoute = (path) => {
     if (path === location.pathname) {
       return true;
@@ -39,11 +48,12 @@ const Header = () => {
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/sign-in") && "text-black border-b-red-500"
+                (pathMatchRoute("/sign-in") || pathMatchRoute("profile")) &&
+                "text-black border-b-red-500"
               }`}
-              onClick={() => navigate("/sign-in")}
+              onClick={() => navigate("/profile")}
             >
-              Sign In
+              {pageState}
             </li>
           </ul>
         </div>
