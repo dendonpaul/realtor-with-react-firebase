e8cb7b1adfb47c5b8264e68cabd1e7f173770433
dendonpaul <dendonpaul@gmail.com>
2022-10-24 05:39:34 +0530

    add share functionlity

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -11,10 +11,12 @@ import SwiperCore, {
   Pagination,
 } from "swiper";
 import "swiper/css/bundle";
+import { FaShare } from "react-icons/fa";
 
 const Listing = () => {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLink, setShareLink] = useState(false);
   const params = useParams();
   // SwiperCore.use(Autoplay, Navigation, Pagination);
   useEffect(() => {
@@ -53,6 +55,23 @@ const Listing = () => {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        className="fixed top-[13%] right-[3%] z-10 cursor-pointer bg-white border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLink(true);
+          setTimeout(() => {
+            setShareLink(false);
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-slate-500" />
+        {shareLink && (
+          <span className="absolute top-[50px] right-6 border-2 border-gray-400 whitespace-nowrap text-sm bg-gray-100 p-2">
+            Link Copied!
+          </span>
+        )}
+      </div>
     </main>
   );
 };
